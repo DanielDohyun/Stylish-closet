c7454bf97870efc935fe7c97e25d7f4346119009
DanielKimm <66330126+DanielKimm@users.noreply.github.com>
2020-09-08 19:22:33 -0400

    start footer styling

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -12,26 +12,29 @@ class Footer extends Component {
 
       <>
         <div className="footer">
-          <div class="sns">
-            <h1 class="sns__heading">Get In Touch</h1>
-            <div class="sns__container">
-              <img class="sns__icon" src={instagram} />
-              
-              <img class="sns__icon" src={facebook} />
-              <img class="sns__icon" src={twitter} />
+          <div className="footer__inner">
+            <div class="sns">
+              <h1 class="sns__heading">Get In Touch</h1>
+              <div class="sns__container">
+                <img class="sns__icon" src={instagram} />
+                
+                <img class="sns__icon" src={facebook} />
+                <img class="sns__icon" src={twitter} />
+              </div>
             </div>
-          </div>
 
-          <div class="contact">
-            <div class="contact__container">
-              <p class="contact__name">Richard Rovner
-                The Bees Knees Management</p>
-              <p class="contact__location">460 King Street West
-                Toronto, Ontario M5V 1L7</p>
-              <p class="contact__email"><a href="mailto:info@thebeesknees.com">info@thebeesknees.com</a></p>
+            <div class="contact">
+              <div class="contact__container">
+                <h1 className="contact__heading">Support</h1>
+                <p class="contact__name">
+                  Stylish Closet Management</p>
+                <p class="contact__location">1000 Style Street West
+                  Toronto, Ontario M5V 1L7</p>
+                <p class="contact__email"><a className="contact__mailto" href="mailto:info@stylishcloset.com">info@stylishcloset.com</a></p>
+              </div>
             </div>
           </div>
-            <p class="copyright">Copyright The Bees Knees © 2018
+            <p class="copyright">Copyright The Stylish Closet © 2020
               All Rights Reserved</p>
         </div>
 
